16887f8d7b8523c7f633ef93e78d04db4cf834b0
Joey Thao <thaojoey5@gmail.com>
2023-11-27 23:50:00 -0600

    finish ProductTag.js

--- Develop/models/ProductTag.js
+++ Develop/models/ProductTag.js
@@ -6,7 +6,29 @@ class ProductTag extends Model {}
 
 ProductTag.init(
   {
-    // define columns
+    // Below: the product id will reference the product and tag id will reference the tag. 
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    product_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'product',
+        key: 'id',
+        unique: false
+      }
+    },
+    tag_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'tag',
+        key: 'id',
+        unique: false
+      }
+    }
   },
   {
     sequelize,
